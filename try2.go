c61f81859fedb57d2f5dc4385221c1dbdaf53e95
“zinaly2004” <jannatunferdousezinaly2004@gmail.com>
2023-03-15 14:09:04 +0600

    2nd commit

---
+++ try2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a bool = false
+	var b int = 4
+	var c float32 = 3.23
+	var d string = "zinaly"
+
+	fmt.Println("boolean: ", a)
+	fmt.Println("int:", b)
+	fmt.Println("float", c)
+	fmt.Println("string: ", d)
+	
+}
