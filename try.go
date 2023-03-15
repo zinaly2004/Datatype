9a353823d58361d640dcba84e33f1a3979d7db16
“zinaly2004” <jannatunferdousezinaly2004@gmail.com>
2023-03-15 14:00:31 +0600

    first commit

---
+++ try.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a bool = true
+	var b int = 4
+	var c float32 = 1.23
+	var d string = "hyi"
+
+	fmt.Println("boolean: ", a)
+	fmt.Println("int:", b)
+	fmt.Println("float", c)
+	fmt.Println("string: ", d)
+	
+}
